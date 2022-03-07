2039b565fcf423bf0224676e9c502ea8582219c6
zhangjun <ewalker@live.cn>
2022-03-07 09:08:06 +0000

    update

--- inference_benchmark.sh
+++ inference_benchmark.sh
@@ -1,9 +1,10 @@
 
-backend_type=(onnxruntime paddle)
-enable_trt=(true false)
-enable_gpu=(true false)
+backend_type_list=(onnxruntime paddle)
+enable_trt_list=(true false)
+enable_gpu_list=(true false)
+enable_mkldnn_list=(true false)
 gpu_id=0
-batch_size=(1 8 16)
+batch_size_list=(1 8 16)
 config_file=config.yaml
 export BASEPATH=$(cd `dirname $0`; pwd)
 export MODELPATH="$BASEPATH/Models"
@@ -30,8 +31,18 @@ run_benchmark(){
           echo "find param file: $params_file"
         fi
     done
-    for batch_size in ${batch_size[@]};do
-      python benchmark.py --model_dir=${model_dir} --config_file ${config_file} --enable_gpu=true --gpu_id=0 --enable_trt=false --backend_type=${backend_type} --batch_size=${batch_size} --paddle_model_file "$model_file" --paddle_params_file "$params_file"
+    for batch_size in ${batch_size_list[@]};do
+      for enable_gpu in ${enable_gpu_list[@]};do
+        if [ ${enable_gpu} = "true" ]; then
+          for enable_trt in ${enable_trt_list[@]};do
+            python benchmark.py --model_dir=${model_dir} --config_file ${config_file} --enable_gpu=${enable_gpu} --gpu_id=${gpu_id} --enable_trt=${enable_trt} --backend_type=${backend_type} --batch_size=${batch_size} --paddle_model_file "$model_file" --paddle_params_file "$params_file"
+          done
+        elif [ ${enable_gpu} = "false" ]; then
+          for enable_mkldnn in ${enable_mkldnn_list[@]};do
+            python benchmark.py --model_dir=${model_dir} --config_file ${config_file} --enable_mkldnn=${enable_mkldnn} --enable_gpu=false --gpu_id=0 --enable_trt=false --backend_type=${backend_type} --batch_size=${batch_size} --paddle_model_file "$model_file" --paddle_params_file "$params_file"
+          done
+        fi
+      done
     done
   done
 }
