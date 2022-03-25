722af9ebc405dc581482c6893df20464d3641cbf
yeliang2258 <1047690002@qq.com>
2022-03-25 02:16:05 +0000

    uninstall paddle2onnx first

--- onnx_convert.sh
+++ onnx_convert.sh
@@ -2,6 +2,7 @@ export PYTHON_CMD=python
 export BASEPATH=$(cd `dirname $0`; pwd)
 export MODELPATH="$BASEPATH/Models"
 install_repo(){
+  $PYTHON_CMD -m pip uninstall paddle2onnx
   cd "$BASEPATH"
   rm -rf Paddle2ONNX
   git clone https://github.com/PaddlePaddle/Paddle2ONNX.git
